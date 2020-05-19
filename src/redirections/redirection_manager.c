83c82a8b7248752ce3956c28f8792628c203c5bd
Clément Le Bihan <clement.le-bihan@epitech.eu>
2020-05-19 14:32:44 +0200

    init of parser branch

--- src/redirections/redirection_manager.c
+++ src/redirections/redirection_manager.c
@@ -93,6 +93,11 @@ int command_format_is_invalid(char **cmds, env_t *env, int *return_values)
     return (0);
 }
 
+int parser()
+{
+    //did this for init commit
+}
+
 int eval_raw_cmd(char *cmd, env_t *env)
 {
     int *return_values = NULL;
