04403899b834ec1e046b76185c40c1045e4a49ea
Clément Le Bihan <clement.le-bihan@epitech.eu>
2020-05-20 11:24:46 +0200

    starting the inhibitors

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -22,21 +22,66 @@ const parser_map parsers[] = {
     {NULL, NULL}
 };
 
-char **parse_input(char *cmd)
+/*char **parse_input(char *cmd)
 {
     char *data;
-    char **ret = malloc(sizeof(char *) * 2);
+    char **ret = malloc(sizeof(char *) * (strlen(cmd) + 1));
     int return_val = 0;
     int inc = 0;
+    char *ptr = cmd;
+    int inc_for = 1;
 
-    for (int i = 0; cmd[i]; i++) {
-        for (int j = 0; parsers[j].key; j++) {
-            if (cmd[i] == parsers[j].key)
-                return_val = parsers[j].parser(&cmd[i], &data);
-            if (return_val == -1)
-                return (NULL);
-            ret[inc++] = data;
-            i += return_val;
+    for (int i = 0; cmd[i]; i++, inc_for++) {
+        if (cmd[i] >= 33 && cmd[i] <= 126)
+            continue;
+        if (inc_for == 1) {
+            inc_for = 0;
+            continue;
         }
+        
     }
+}*/
+
+bool is_character_valid(char c)
+{
+    static bool is_valid = false;
+
+    if (is_valid) {
+        is_valid = false;
+        return (true);
+    }
+    if (c == '\\') {
+        is_valid = true;
+    }
+    if (c >= 33 && c <= 126)
+        return (true);
+    return (false);
+}
+
+char **parse_input(char *cmd)
+{
+    int size = strlen(cmd);
+    char **ret = malloc(sizeof(char *) * (size + 1));
+    char *ptr = cmd;
+    int ret_inc = 0;
+    char *new;
+
+    for (int i = 0, inc = 1; i <= size; i++, inc++) {
+        if (is_character_valid(cmd[i]))
+            continue;
+        if (inc == 1) {
+            ptr = cmd + i + 1;
+            inc = 0;
+            continue;
+        }
+        new = strndup(ptr, inc);
+        if (!new)
+            return (NULL);
+        new[strlen(new) - 1] = '\0';
+        ret[ret_inc++] = new;
+        ptr = cmd + i + 1;
+        inc = 0;
+    }
+    ret[ret_inc] = NULL;
+    return (ret);
 }
